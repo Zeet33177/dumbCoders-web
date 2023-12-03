6d142c67e93ee8120a6c4a6c8be12b78727d057f
Abhijeet Bhattacharya <115207805+Zeet33177@users.noreply.github.com>
2023-12-03 11:42:00 +0530

    Initial Setup
    Loading the .html, .css and .js files

---
+++ app.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    let e = document.body;
+    e.classList.toggle("dark-mode");
+}
+
+function run() {
+    let ham = document.getElementsById("runner");
+    console.log(ham);
+    ham.style.visibility = "visible";
+}

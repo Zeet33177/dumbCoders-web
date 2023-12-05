d15823ab2c495898fbdfccbfce519a48290c2423
Zeet33177 <abhijeet33177@gmail.com>
2023-12-05 08:22:14 +0530

    updated dark mode and text section

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-function myFunction() {
-    let e = document.body;
-    e.classList.toggle("dark-mode");
-}
-
-function run() {
-    let ham = document.getElementsById("runner");
-    console.log(ham);
-    ham.style.visibility = "visible";
-}
+const toggle = document.getElementById('sun');
+const body = document.querySelector('body');
+console.log(body)
+toggle.addEventListener('click', function () {
+    this.classList.toggle('fa-moon');
+    if (this.classList.toggle('fa-sun')) {
+        body.style.setProperty('--bgcolor', 'paleturquoise')
+        body.style.setProperty('--primarycolor', 'lightseagreen')
+        body.style.setProperty('--bordercolor', 'black')
+        body.style.transition = '1s';
+    }
+    else {
+        body.style.setProperty('--bgcolor', 'black')
+        body.style.setProperty('--primarycolor', '#198c8c')
+        body.style.setProperty('--bordercolor', 'white')
+        body.style.transition = '1s';
+    }
+})
